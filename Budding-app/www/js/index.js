e0674ab8eae2d204be3a7a1812cfd0bd4d70235f
masonhill392 <masonhill392@gmail.com>
2022-12-01 08:50:50 -0800

    Loading screen, main screen

--- Budding-app/www/js/index.js
+++ Budding-app/www/js/index.js
@@ -1,8 +1,41 @@
-
-document.addEventListener('deviceready', onDeviceReady, false);
-
-function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
+console.log("js connected");
+circleLoadingAnimation();
 
 
+
+
+
+
+
+
+
+
+function circleLoadingAnimation() {
+    x=1
+    while (x != 6) {
+        y = 0.1;
+
+        while (y <= 1) {
+            
+            updateOpacity();
+
+        }
+        x++;
+    }
+    if (x = 6) {
+        $("#splash-body").hide();
+        $("#main-body").show();
+        console.log("Finished loading. Main page showing.")
+    }
+
+
+
+    function updateOpacity() {
+        y = y+0.1;
+        console.log("Circle " + x + "|" + y);
+        $("#circle" + x).css("filter", "opacity(" + y + ")");
+    }
 }
+    
+
+
