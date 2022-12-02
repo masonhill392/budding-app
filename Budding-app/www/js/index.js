28a459452a4b42299c26e2697ea3fa37a2f9c5fb
masonhill392 <masonhill392@gmail.com>
2022-12-02 13:13:23 -0800

    Matrix / Nested Array test

--- Budding-app/www/js/index.js
+++ Budding-app/www/js/index.js
@@ -33,76 +33,115 @@ console.log("js connected");
 // });
 
 
+var firstname = "null";
+var lastname = "null";
+var age = "0";
+var eye = "red";
+
+
+// array
+// const pets = ["person1", "person2"];
+
+//This is how I should set up pets & their respective properties. It is called "Nested Arrays" - arrays within arrays. Javascript objects within an array.
+//pets[0][0] = Get property within the first array's ([0]) first array([0]). Output is "Tim"
+//pets[0][2] = Get property within the first array's ([0]) second array([2]). Output is "cat"
+//"pets" is the primary array. Within "pets", contains arrays with ID's ([0], [1]). These ID's contain properties, such as the name, breed, age, and type of pet.
+//https://www.elated.com/nested-arrays-in-javascript/#:~:text=To%20access%20the%20elements%20of,element%20of%20the%20pets%20array.
+var pets = new Array ( );
+pets[0] = new Array ( "Tim", "Orange Tabby", " cat" );
+pets[1] = new Array ( "Jasper", 12, "dog" );
+console.log( pets[0][0] + " is an " + pets[0][1] + pets[0][2]); // Displays "Tim is an Orange Tabby cat"
+console.log( pets[1][0] + " is a " + pets[1][1] + " year old " + pets[1][2] ); // Displays "Jasper is a 12 year old dog"
+
+
+
+
 $("#main-view").click(function(){
-    console.log("toggled")
     $("#selectors").toggle();
-    getVariables();
-    console.log("1")
+    // getVariables();
+    // $("#main-view").after("<p>" + firstname + lastname + age + eye + "</p>");
 });
 
 
-var pname;
-var ptype;
-var plocation;
-var pcoordinates;
-var ptimer;
+$("#").click(function() {
 
-// function pet1(petName, petType, petLocation, petCoordinates, petTimer) {
+    storePetInHTML();
 
-//     this.petName = pname;
-//     this.petType = ptype;
-//     this.petLocation = plocation;
-//     this.petCoordinates = pcoordinates;
-//     this.petTimer = ptimer;
+});
+
+
+function storePetInHTML() {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var pname;
+// var ptype;
+// var plocation;
+// var pcoordinates;
+// var ptimer;
+
+
+
+// function getVariables() {
+//     currentpet = pet1;
+//     currentpet.petName = pname;
+//     currentpet.petType = ptype;
+//     currentpet.petLocation = plocation;
+//     currentpet.petCoordinates = pcoordinates;
+//     currentpet.petTimer = ptimer;
+//     $("#pet-name").html(pname)
+//     console.log(pname);
+//     console.log("setDisplayPet ran")
 //     return;
 // }
 
+// function Pet(petName, petType) {
+//     this.petName = pname;
+//     this.petType = ptype;
+// }
+// const myPet = new Pet("Catty", "Cat")
 
-function getVariables() {
-    currentpet = pet1;
-    currentpet.petName = pname;
-    currentpet.petType = ptype;
-    currentpet.petLocation = plocation;
-    currentpet.petCoordinates = pcoordinates;
-    currentpet.petTimer = ptimer;
-    $("#pet-name").html(pname)
-    console.log(pname);
-    console.log("setDisplayPet ran")
-    return;
-}
+// var locations = [
+//     "home",
+//     "gym",
+//     "library"
+// ]
 
-function Pet(petName, petType) {
-    this.petName = pname;
-    this.petType = ptype;
-}
-const myPet = new Pet("Catty", "Cat")
+// var pet1 = {
+//     petName: "Jambo",
+//     petType: "Cat",
+//     petLocation: "home",
+//     petCoordinates: "test",
+//     petTimer: "60"
+// }
 
-var locations = [
-    "home",
-    "gym",
-    "library"
-]
 
-var pet1 = {
-    petName: "Jambo",
-    petType: "Cat",
-    petLocation: "home",
-    petCoordinates: "test",
-    petTimer: "60"
-}
+// var pet2 = {
+//     petName: "Bacon",
+//     petType: "Dog",
+//     petLocation: "gym",
+//     petCoordinates: "test",
+//     petTimer: "120"
+// }
 
-var pet2 = {
-    petName: "Bacon",
-    petType: "Dog",
-    petLocation: "gym",
-    petCoordinates: "test",
-    petTimer: "120"
-}
-
-var pet3 = {
-    petName: "Prick",
-    petType: "Toucan",
-    petLocation: "Library",
-    petCoordinates: "test" ,
-    petTimer: "240"
-}
+// var pet3 = {
+//     petName: "Prick",
+//     petType: "Toucan",
+//     petLocation: "Library",
+//     petCoordinates: "test" ,
+//     petTimer: "240"
+// }
