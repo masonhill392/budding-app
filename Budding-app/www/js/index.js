d959377f9f03328e0bcf9ab0afa9b80f258d9102
masonhill392 <masonhill392@gmail.com>
2022-12-03 06:53:20 -0800

    Forward / Backward Buttons

--- Budding-app/www/js/index.js
+++ Budding-app/www/js/index.js
@@ -4,7 +4,6 @@ $("#splash-body").hide();
 $("#selectors").show();
 
 // circleLoadingAnimation();
-
 // function circleLoadingAnimation() {
 //     x=1
 //     while (x != 6) {
@@ -26,55 +25,54 @@ $("#selectors").show();
 //     }
 // }
     
-// $("#see-your-buddies").click(function() {
-
-//     console.log("see-your-buddies clicked");
-//     $("#intro-section").hide()
-//     $("#main-view").show()
-
-// });
 
 
-var firstname = "null";
-var lastname = "null";
-var age = "0";
-var eye = "red";
-
-
-// array
-// const pets = ["person1", "person2"];
-
-//https://www.elated.com/nested-arrays-in-javascript/#:~:text=To%20access%20the%20elements%20of,element%20of%20the%20pets%20array.
-
-
-
-
-var pets = new Array ( );
-//------------------( Name,  Animal type, Animal breed, Timer, Activity)
-pets[0] = new Array ( "Jambo", "Cat","Orange Tabby", 60, "Studying");
-pets[1] = new Array ( "Bacon", "Dog","Bichon Shih-tzu", 120, "Gym");
-console.log( pets[0][0] + " is an " + pets[0][2] +" "+ pets[0][1] + ". " + pets[0][0] +" enjoys "  + pets[0][4] + " with you every " + pets[0][3] + " seconds.");
 
 x = 0;
 var i = 0;
 var z = 0;
 
+//https://www.elated.com/nested-arrays-in-javascript/#:~:text=To%20access%20the%20elements%20of,element%20of%20the%20pets%20array.
+var pets = new Array ( );
+pets[0] = new Array ( "Jambo", "Cat","Orange Tabby", 60, "Studying");
+pets[1] = new Array ( "Bacon", "Dog","Bichon Shih-tzu", 120, "Gym");
+pets[2] = new Array ( "Spot", "Dog","Dalmation", 130, "Reading");
+pets[3] = new Array ( "Red", "Cat","Tuxedo", 90, "Cleaning");
+pets[4] = new Array ( "Duke", "Dog","Mastiff", 360, "Eating");
+pets[5] = new Array ( "Nearo", "Dog","Burmese Mountain Dog", 240, "Meditating");
+var length = Object.keys(pets).length;
 
-$("#baconbutton").click(function() {
 
-    var length = Object.keys(pets).length;
+init();
 
-    if(i < length ) {//PET TARGETER
-        i++;
-    }
-    if(i >= length) {
-        i = 0;
-    }
-    updatePetInHTML();
+$(".item1").click(function(event){
+    i = event.target.id;
+    updatePetInHTML()
 });
 
+
+$("button").click(function(event) {//FORWARD & BACK BUTTONS
+    if(event.target.id == "forward") {
+    if(i < length-1 ) {i++;}else if(i = length ) {i = 0;}
+        updatePetInHTML();
+    }
+    if(event.target.id == "backward") {
+        if(i > 0) {i--;}else if(i == 0 ){i = length - 1;}
+        updatePetInHTML();
+    }
+});
+
+
+
+
+function init() {
+    for(let z = 0; z < length; z++) {
+        $("#location-group").append("<button class='item1' id='"+z+"'>"+pets[z][0]+"</button>")
+    }
+}
+
 function updatePetInHTML() {
-    console.log(z);
+    console.log(i)
         $("#pet-name").html(pets[i][0]);
         $("#pet-type").html(pets[i][1]);
         $("#pet-breed").html(pets[i][2]);
@@ -83,8 +81,3 @@ function updatePetInHTML() {
 }
 
 
-
-
-    // $("#pet-name").html(pets[0][0]);
-    // var test = pets[x][0];
-    // console.log(test);
