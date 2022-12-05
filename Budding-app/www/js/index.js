bfcec69e54e7a6209e9bf026bfe2b29fe25a2354
masonhill392 <masonhill392@gmail.com>
2022-12-05 07:25:38 -0800

    Update Dec. 5th

--- Budding-app/www/js/index.js
+++ Budding-app/www/js/index.js
@@ -1,7 +1,32 @@
-$("#intro").hide();
-$("#main-view").show();
-$("#splash-body").hide();
-$("#selectors").show();
+
+    $("#splash-body").hide();
+    $("#intro").hide();
+    $("#main-view").show();
+    $("#overlay").hide();
+
+
+
+// intro();
+
+
+// function intro() {
+//     $("#splash-body").show();
+//     $("#intro").hide();
+//     $("#main-view").hide();
+//     $("#overlay").hide();
+//     setInterval(showIntro, 3000);
+// }
+// function showIntro() {
+//     $("#splash-body").hide();
+//     $("#intro").show();
+//     setInterval(showMainView, 5000);
+
+// }
+// function showMainView() {
+//     $("#intro").hide();
+//     $("#main-view").show();
+// };
+// $("#selectors").show();
 
 // circleLoadingAnimation();
 // function circleLoadingAnimation() {
@@ -25,50 +50,47 @@ $("#selectors").show();
 //     }
 // }
     
-
-
-
 x = 0;
 var i = 0;
 var z = 0;
 
 //https://www.elated.com/nested-arrays-in-javascript/#:~:text=To%20access%20the%20elements%20of,element%20of%20the%20pets%20array.
 var pets = new Array ( );
-pets[0] = new Array ( "Jambo", "Cat","Orange Tabby", 60, "Studying");
-pets[1] = new Array ( "Bacon", "Dog","Bichon Shih-tzu", 120, "Gym");
-pets[2] = new Array ( "Spot", "Dog","Dalmation", 130, "Reading");
-pets[3] = new Array ( "Red", "Cat","Tuxedo", 90, "Cleaning");
-pets[4] = new Array ( "Duke", "Dog","Mastiff", 360, "Eating");
-pets[5] = new Array ( "Nearo", "Dog","Burmese Mountain Dog", 240, "Meditating");
+pets[0] = new Array ( "Jambo", "Cat","Orange Tabby", 1, "Studying");
+pets[1] = new Array ( "Bacon", "Dog","Bichon Shih-tzu", 2, "Gym");
+pets[2] = new Array ( "Spot", "Dog","Dalmation", 3, "Reading");
+pets[3] = new Array ( "Red", "Cat","Tuxedo", 4, "Cleaning");
+pets[4] = new Array ( "Duke", "Dog","Mastiff", 5, "Eating");
+pets[5] = new Array ( "Nearo", "Dog","Burmese Mountain Dog", 6, "Meditating");
 var length = Object.keys(pets).length;
-
 
 init();
 
-$(".item1").click(function(event){
-    i = event.target.id;
-    updatePetInHTML()
-});
 
 
 $("button").click(function(event) {//FORWARD & BACK BUTTONS
+    length = Object.keys(pets).length;
+
     if(event.target.id == "forward") {
     if(i < length-1 ) {i++;}else if(i = length ) {i = 0;}
         updatePetInHTML();
+        console.log(i)
     }
     if(event.target.id == "backward") {
         if(i > 0) {i--;}else if(i == 0 ){i = length - 1;}
         updatePetInHTML();
+        console.log(i)
     }
 });
 
-
-
-
 function init() {
+    length = Object.keys(pets).length;
+    console.log(length + "=length")
     for(let z = 0; z < length; z++) {
+        console.log("z = " + z + "|length = " + length);
         $("#location-group").append("<button class='item1' id='"+z+"'>"+pets[z][0]+"</button>")
     }
+    console.log("does pets[6] exist? " + pets[6]);
 }
 
 function updatePetInHTML() {
@@ -76,8 +98,89 @@ function updatePetInHTML() {
         $("#pet-name").html(pets[i][0]);
         $("#pet-type").html(pets[i][1]);
         $("#pet-breed").html(pets[i][2]);
-        $("#pet-timer").html(pets[i][3]);
+        $("#pet-timer").html(pets[i][3] + " seconds");
         $("#pet-activity").html(pets[i][4]);
 }
+
+$("#petpanelbutton").click(function() {
+    $("#selectors").toggle();
+    $("#overlay").hide();
+})
+$("#petbutton").click(function() {
+    $("#overlay").toggle();
+    $("#selectors").hide();
+})
+$("#newPet").click(function(){
+    $("#overlay").hide();
+    $("#selectors").hide();
+    $("#newPetMenu").show();
+})
+$("#submitNewPet").click(function() {
+    $("#newPetMenu").hide();
+    $("#selectors").show();
+    q = Object.keys(pets).length;
+    console.log(q);
+    var name = document.getElementById('petNameInput').value;
+    var type = document.getElementById('petTypeInput').value;
+    var breed = document.getElementById('petBreedInput').value;
+    var actLength = document.getElementById('petActivityInput').value;
+    var act = document.getElementById('petActivityLengthInput').value;
+    pets[q] = new Array ( name, type, breed, actLength, act);
+    console.log(pets[q]);
+    $("#location-group").append("<button class='item1' id='"+q+"'>"+pets[q][0]+"</button>")
+    i = q;
+    updatePetInHTML()
+
+
+
+
+
+})
+
+//ENCOUNTERED A DUMB ERROR HERE
+//Dynamically created elements cannot be detected by .click handler. Must be attached to something natively HTML, & use .on("click")
+
+$("#location-group").on("click", function(event){
+    console.log("clicked");
+    i = event.target.id;
+    console.log(i)
+    updatePetInHTML()
+});
+// $("#startTimer").click(function() {
+//     $("#startTimer").hide();
+//     $("#stopTimer").show();
+//     timer = 60*pets[i][3];
+//     console.log(timer)
+//     setInterval(activityTimer, 300);
+        
+//     });
+
+//     $("#stopTimer").click(function() {
+//         $("#startTimer").show();
+//         $("#stopTimer").hide();
+//         clearInterval(activityTimer);
+            
+//         });
+
+
+    
+// https://stackoverflow.com/questions/31106189/create-a-simple-10-second-countdown
+
+
+// function activityTimer() {  
+//     if (timer > 0) {
+//         timer--;
+//         console.log(timer)
+//         $("#timerSlot").html(timer);
+//     }
+//     if(timer == 0) {
+//         console.log(timer)
+//         $("#timerSlot").html('Finished');
+        
+//     }
+    
+// }
+
+
 
 
